e36093e6d0b77468ea99ce004fa0cc6e1a3fc8da
Baek Namgyu <parkiy7611@gmail.com>
2025-04-13 11:24:25 +0900

    Fix: static files + swagger auth

--- config/settings.py
+++ config/settings.py
@@ -121,7 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # ← 배포용 정적파일 저장 폴더
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
